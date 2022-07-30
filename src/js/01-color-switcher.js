a33911dc46de4001156f53364cfa54a8ec674491
Uliana Vasylchenko <ulianabarnas9225@gmail.com>
2022-07-30 02:12:26 +0200

    start task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  const intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
