9c668868e2368e4f880e543851549f8cabad0ae1
Uliana Vasylchenko <ulianabarnas9225@gmail.com>
2022-08-05 14:07:19 +0200

    fix task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,58 +10,38 @@ function onFormSubmit(e) {
   e.preventDefault();
   if (isActive) return;
 
-  const delay = +form.elements.delay.value;
+  let delay = +form.elements.delay.value;
   const step = +form.elements.step.value;
   const amount = +form.elements.amount.value;
   isActive = true;
 
-  setTimeout(
-    () => {
-      let position = 1;
-      let intervalDelay = delay + step;
+  for (let i = 1; i <= amount; i += 1) {
+    console.log(i);
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
 
-      getPromiseNotification(position, delay);
-
-      const intervalId = setInterval(() => {
-        if (position >= amount) {
-          clearInterval(intervalId);
-          isActive = false;
-          return;
-        }
-
-        position += 1;
-
-        getPromiseNotification(position, intervalDelay);
-
-        intervalDelay += step;
-      }, step);
-    },
-    delay,
-    step,
-    amount
-  );
+    delay += step;
+  }
+  isActive = false;
 }
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
-}
-
-function getPromiseNotification(position, delay) {
-  createPromise(position, delay)
-    .then(({ position, delay }) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
 }
 
 Notify.init({
